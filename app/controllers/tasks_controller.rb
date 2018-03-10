1d80bfdcf86bc439af7a0b231d1f6957ee9ba7da
yoshiko.tsuka <ec2-user@ip-172-31-7-86.us-east-2.compute.internal>
2018-03-10 07:51:24 +0000

    CRUD tasks

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,62 @@
+class TasksController < ApplicationController
+  before_action :setting_task, only: [:show, :edit, :update, :destroy]
+  def index
+    @tasks = Task.order(created_at: :desc).page(params[:page]).per(50)
+  end
+
+  def show
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+
+    if @task.save
+      flash[:success] = 'task is registed!'
+      redirect_to @task
+    else
+      flash.now[:danger] = 'failure to regist'
+      render :new
+    end
+  end
+  
+  def edit
+  end
+
+  def update
+    if @task.update(task_params)
+      flash[:success] = 'task is updated!'
+      redirect_to @task
+    else
+      flash.now[:danger] = 'failure to update '
+      render :edit
+    end
+  end
+
+  def destroy
+    
+    if @task.destroy
+      flash[:success] = 'task is deleted!'
+      redirect_to tasks_url
+    else
+      flash.now[:danger] = 'failure to delete '
+      render :edit
+    end
+  end
+  
+  private
+
+  # Strong Parameter
+  
+  def setting_task
+    @task = Task.find(params[:id])
+  end
+  
+  def task_params
+    params.require(:task).permit(:content)
+  end
+  
+end
